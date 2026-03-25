baef99ab6da3f87499e53f3eeb4ee758484083e6
SophiaLM <106804291+SophiaLM@users.noreply.github.com>
2026-03-25 15:32:46 +0100

    Add files via upload

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -5,6 +5,7 @@ Usage:
 """
 
 from __future__ import annotations
+from cli import main
 
 import os
 import sys
@@ -13,9 +14,6 @@ ROOT_DIR = os.path.dirname(__file__)
 SRC_DIR = os.path.join(ROOT_DIR, "src")
 if SRC_DIR not in sys.path:
     sys.path.insert(0, SRC_DIR)
-
-from cli import main
-
 
 if __name__ == "__main__":
     raise SystemExit(main())
